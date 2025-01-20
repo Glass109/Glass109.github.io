e4c4045001469f3c2d7db8a089e04e1a144faf75
GitHub Action <action@github.com>
2025-01-20 17:24:36 -0600

    Changed base_url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: true,
+    app: {
+        baseURL: '/glass109/', // Set the base path for GitHub Pages
+    },
     compatibilityDate: '2024-11-01',
-    devtools: {enabled: true},
+    devtools: { enabled: true },
     modules: [
-      '@nuxtjs/tailwindcss',
-      '@vueuse/nuxt',
-      '@nuxt/fonts',
-      '@nuxt/scripts',
-      '@nuxt/icon'
+        '@nuxtjs/tailwindcss',
+        '@vueuse/nuxt',
+        '@nuxt/fonts',
+        '@nuxt/scripts',
+        '@nuxt/icon',
     ],
     tailwindcss: {
         config: {
-            content: [
-                './public/dictionaries/*.{js,json,ts}'
-            ]
-        }
+            content: ['./public/dictionaries/*.{js,json,ts}'],
+        },
     },
     nitro: {
-        baseURL: 'https://glass109.github.io',
-        siteName: 'Glass109',
         prerender: {
             crawlLinks: true,
             failOnError: false,
-        }
+        },
     },
-    generate:{
+    generate: {
         nojekyll: true,
-        fallback: '404.html'
+        fallback: '404.html',
     },
     target: 'static',
-
-})
+});
